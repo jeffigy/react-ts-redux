fb934333ed5429c36fb3f6348ec080df27835346
jeffigy <ejeffydev@gmail.com>
2023-11-30 13:27:05 +0800

    new project setup

---
+++ 5. RTK Query CRUD example/src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
+import { ChakraProvider } from "@chakra-ui/react";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
