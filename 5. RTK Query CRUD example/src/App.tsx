5b87a19e18bac74cabbd94284cdb9104e9b30f80
jeffigy <ejeffydev@gmail.com>
2023-12-01 09:18:57 +0800

    crud finish

--- 5. RTK Query CRUD example/src/App.tsx
+++ 5. RTK Query CRUD example/src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
+import TodoList from "./features/todos/TodoList";
 
-type AppProps = {};
-
-const App: React.FC<AppProps> = () => {
-  return <div>Have a good coding</div>;
+const App = () => {
+  return <TodoList />;
 };
 export default App;
--- 5. RTK Query CRUD example/src/main.tsx
+++ 5. RTK Query CRUD example/src/main.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./features/api/apiSlice.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </BrowserRouter>
-    </Provider>
+    <ApiProvider api={apiSlice}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ApiProvider>
   </React.StrictMode>
 );
