7a0a063683f624ce49e97ca93e9b2549ff011bc2
jeffigy <ejeffydev@gmail.com>
2024-05-31 22:44:15 +0800

    feat: Added Reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,6 +1,6 @@
 const BUY_CAKE = "BUY_CAKE";
 
-//Action Creator
+// Action Creator
 function buyCake() {
   // Action
   return {
@@ -8,3 +8,22 @@ function buyCake() {
     info: "first redux action",
   };
 }
+
+// Reducer
+// (previousState, action) => newState
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
