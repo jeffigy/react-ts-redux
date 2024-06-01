85c187c06896f5d5abb29d8081298b8ed4150cf7
jeffigy <ejeffydev@gmail.com>
2024-06-01 13:35:50 +0800

    feat: Added Store

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,15 +1,18 @@
+const redux = require("redux");
+const createStore = redux.legacy_createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
 // Action Creator
 function buyCake() {
-  // Action
+  //* Action
   return {
     type: BUY_CAKE,
     info: "first redux action",
   };
 }
 
-// Reducer
+//* Reducer
 // (previousState, action) => newState
 const initialState = {
   numberOfCakes: 10,
@@ -27,3 +30,18 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+//* Store
+// creates store
+const store = createStore(reducer);
+console.log("initial state", store.getState());
+// subscribe() registers a listener function
+// subscribe() will be called every time an action is dispatched to the store
+store.subscribe(() => console.log("updated state", store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+// we need to unsubscribe() at the end to prevent
+// memory leaks/Avoid Unnecessary Computations or to clean up
+unsubscribe();
