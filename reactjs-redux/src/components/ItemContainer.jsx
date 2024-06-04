5bce267edd98b7ef65f2f9ccec10cb4c1271e91c
jeffigy <ejeffydev@gmail.com>
2024-06-04 13:53:20 +0800

    feat: added mapDispatchToProps

--- reactjs-redux/src/components/ItemContainer.jsx
+++ reactjs-redux/src/components/ItemContainer.jsx
@@ -1,9 +1,11 @@
 import { connect } from "react-redux";
+import { buyCake, buyIceCream } from "../redux";
 
 const ItemContainer = (props) => {
   return (
     <div>
       <h2>Item - {props.item}</h2>
+      <button onClick={props.buyItem}>Buy items</button>
     </div>
   );
 };
@@ -18,4 +20,14 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+
+  return {
+    buyItem: dispatchFunction,
+  };
+};
+
+export default connect(null, mapDispatchToProps)(ItemContainer);
